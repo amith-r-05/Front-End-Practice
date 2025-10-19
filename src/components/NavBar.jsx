a73843eae597aadf0f0b42c3ccc0459b0930e65a
Amith R <amithr2005@gmail.com>
2025-10-19 13:09:25 +0530

    feat: Less rude implementation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,10 @@
 const NavBar = () => {
   return (
     <div className="backdrop-blur-3xl bg-blue-500/40 flex p-5 justify-between">
-      <div className="">Hi</div>
-      <div className="">Ninnappa</div>
-      <div className="">Dappa</div>
+      <div className="">Logo</div>
+      <div className="">About</div>
+      <div className="">Contact</div>
+      <div className="">Account</div>
     </div>
   );
 };
