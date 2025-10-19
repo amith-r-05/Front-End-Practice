ae1d66ceff72190bcf88803550ed885ce721e05c
Amith R <amithr2005@gmail.com>
2025-10-19 13:39:37 +0530

    latest updated navbar testing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,7 @@
 const NavBar = () => {
   return (
     <div className="backdrop-blur-3xl bg-blue-500/40 flex p-5 justify-between">
-      <div className="">Logo</div>
-      <div className="">About</div>
-      <div className="">Contact</div>
-      <div className="">Account</div>
-      <div className="">Hello</div>
+      
     </div>
   );
 };
