eab4ce42cf2ec5baf195d0ae3dbcdb9ac3bf0067
Amith R <amithr2005@gmail.com>
2025-10-19 12:54:11 +0530

    feat: updates nakkan

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+  return (
+    <div className="backdrop-blur-3xl bg-blue-500/40 flex p-5 justify-between">
+      <div className="">Hi</div>
+      <div className="">Ninnappa</div>
+      <div className="">Dappa</div>
+    </div>
+  );
+};
+
+export default NavBar;
